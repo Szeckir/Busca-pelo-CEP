27507c17d8bf34df8bda2ad31640ef3955efb9ba
Szeckir <80286620+Szeckir@users.noreply.github.com>
2024-06-02 15:59:30 -0300

    todo codigo pronto

---
+++ src/README.md
@@ -0,0 +1,3 @@
+Simples projeto de buscagem por CEP com HTML, CSS e JS.
+
+O projeto consiste em uma simples página, onde você deverá digitar o CEP que desejas, caso o CEP exista irá apresentar na tela todas informações da região procurada.
---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const inputBusca = document.querySelector('#input-cep')
+const btnBusca = document.querySelector('#btn-busca-cep')
+const dadosBuscados = document.querySelector('.data-cep')
+
+function searchCep() {
+    const cep = inputBusca.value;
+    fetch(`http://viacep.com.br/ws/${cep}/json/`)
+    .then(data => {
+        if(!data.ok){
+            throw new Error('Erro na conexão com internet')
+        }
+        return data.json();
+    })
+    .then(data => {
+        apareceNaTela(data)
+    })
+}
+
+function apareceNaTela(dados) {
+    inputBusca.value = ''
+    dadosBuscados.innerHTML = ''
+
+    dadosBuscados.innerHTML += `
+        <ul class="data-list">    
+            <li><b>RUA: </b>${dados.logradouro}</li>
+            <li><b>BAIRRO: </b>${dados.bairro}</li>
+            <li><b>CIDADE: </b>${dados.localidade}</li>
+            <li><b>ESTADO: </b>${dados.uf}</li>
+            <li><b>DDD: </b>${dados.ddd}</li>
+        </ul>
+    `
+}
+
